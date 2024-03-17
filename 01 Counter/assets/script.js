6d46104aa57c6aa5e6299abf8975e749cf4b5499
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-03-17 17:05:44 -0400

    First commit

---
+++ 01 Counter/assets/script.js
@@ -0,0 +1,23 @@
+let count = 0 // Declares a variable named 'count' and initializes it with the value 0.
+let countEl = document.getElementById("count-el"); // Logs the retrieved HTML element to the console. - There is an id called count-el on the h2 element.
+let saveEl = document.getElementById("save-el");
+console.log(countEl); //Just to display, and debug porpuses.
+function increment() { //defines the name of the function
+    console.log("clicked"); //Just to display, and debug porpuses.
+    count += 1; // increment the variable by 1
+    console.log(count); //Just to display, and debug porpuses.
+    countEl.innerText = count; // Updates the text content of the HTML element stored in countEl to display the updated value of 'count'.
+}
+function save() {
+    console.log(count);
+    alert(`the value ${count} was saved`);
+    let valueDash = count + " - ";
+    saveEl.innerText += valueDash;
+    count = 0;
+    countEl.innerText = count;
+    console.log(count);
+}
+
+
+
+
