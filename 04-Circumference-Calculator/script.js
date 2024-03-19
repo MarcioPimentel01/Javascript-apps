5489bc2b920f0d56709f8d6e660a91ae4ff8ff7a
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-03-19 04:39:58 -0400

    first commit, circumference calculator

---
+++ 04-Circumference-Calculator/script.js
@@ -0,0 +1,15 @@
+// CONST = A VARIABLE THAT CAN'T BE CHANGED.
+// A GOOD EXAMPLE IS TO USE PI TO CALCULATE A CIRCUMFERENCE, BECAUSE OTHERWISE IT'S NO GOING TO BE A PERFEC CIRCLE.
+
+let circumferenceEl = document.getElementById("circumference-result")
+const PI = 3.14159;
+let circumference;
+// radius = window.prompt("please, enter here the radius of a circle");
+
+
+function circumferenceCalc() {
+    let radius = parseFloat(document.getElementById("radius").value);
+    circumference = 2 * PI * radius; //formula
+    console.log(circumference);
+    circumferenceEl.innerText = circumference;
+}
